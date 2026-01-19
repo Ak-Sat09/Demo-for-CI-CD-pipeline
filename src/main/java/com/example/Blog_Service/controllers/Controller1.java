08aa3ac3e7b6702aaec06dd4b55fe5b07c1e7734
Anmol Mehla <anmolmehla1@outlook.com>
2026-01-19 12:55:09 +0530

    New Api Part 1

--- src/main/java/com/example/Blog_Service/controllers/Controller1.java
+++ src/main/java/com/example/Blog_Service/controllers/Controller1.java
@@ -3,18 +3,12 @@ package com.example.Blog_Service.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // ✅ marks this as a REST controller
+@RestController
+
 public class Controller1 {
 
-    // Simple GET endpoint to test
-    @GetMapping("/blogs/test")
-    public String testApi() {
-        return "API is working!";
-    }
-
-    // Another example GET endpoint
-    @GetMapping("/blogs/hello")
-    public String helloApi() {
-        return "Hello from Controller1!";
+    @GetMapping
+    public String helloApi11() {
+        return "Hello from Controller11!";
     }
 }
